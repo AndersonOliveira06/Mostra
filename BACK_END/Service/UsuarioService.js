8f4fb96cbd3bf93d43e7d3935cb0e4d15a3f63a5
Anderson Oliveira <andersonoliveira.cnh@gmail.com>
2023-11-20 19:00:06 -0300

    Atualização no update de imagens

--- BACK_END/Service/UsuarioService.js
+++ BACK_END/Service/UsuarioService.js
@@ -1,29 +1,51 @@
-import { createUserWithEmailAndPassword, signInWithEmailAndPassword, getAuth, updateProfile, deleteUser } from "firebase/auth";
+import { createUserWithEmailAndPassword, signInWithEmailAndPassword, getAuth, updateProfile, deleteUser } from "firebase/auth"
+import { storage } from "../firebase/firebase_config"
+import { ref, getDownloadURL, uploadBytes } from "firebase/storage"
 
 class UsuarioService {
 
     static signUp = (auth, email, password, firstName, lastName, photo, callback) => {
+
         createUserWithEmailAndPassword(
             auth,
             email,
             password
         )
             .then((userCredential) => {
-                const user = userCredential.user;
-                const informacoesAtualizadas = {
-                    displayName: `${firstName} ${lastName}`,
-                    photoURL: photo
-                };
-                updateProfile(user, informacoesAtualizadas)
+                const user = userCredential.user
+
+                // Upload da imagem para o Storage
+                const storageRef = ref(storage, `FotosPerfis/${user.uid}/${photo.name}`)
+                uploadBytes(storageRef, photo)
                     .then(() => {
-                        callback(userCredential)
+
+                        // Obter a URL do download da imagem
+                        getDownloadURL(storageRef)
+                            .then((downloadURL) => {
+
+                                const informacoesAtualizadas = {
+                                    displayName: `${firstName} ${lastName}`,
+                                    photoURL: downloadURL
+                                }
+
+                                // Atualizar o perfil do usuário com as informações e URL da imagem
+                                updateProfile(user, informacoesAtualizadas)
+                                    .then(() => {
+                                        callback(userCredential)
+                                    })
+                                    .catch((error) => console.log("Não foi possível adicionar as infos no perfil do usuário: ", error))
+
+                            })
+                            .catch((error) => console.log("Não foi possível obter a URL da imagem: ", error))
                     })
-                    .catch((error) => console.log(error))
+                    .catch((error) => console.log("Não foi possível realizar o upload do arquivo: ", error))
             })
-            .catch((error) => console.log(error))
+            .catch((error) => console.log("Não foi possível cadastrar usuário: ", error))
     }
 
+
     static signIn = (auth, email, password, callback) => {
+
         signInWithEmailAndPassword(
             auth,
             email,
@@ -35,31 +57,47 @@ class UsuarioService {
             .catch((error) => console.log(error))
     }
 
+
     static updateAccountData = (firstName, lastName, photo, callback) => {
-        const informacoesAtualizadas = {
-            displayName: `${firstName} ${lastName}`,
-            photoURL: photo
-        };
 
         const auth = getAuth()
         const user = auth.currentUser
 
-        if (user !== null){
-            updateProfile(user, informacoesAtualizadas)
-                .then(() => {
-                    callback("Conta Atualizada com Sucesso!")
-                })
-                .catch((error) => console.log(error))
-        }
+        // Upload da imagem para o Storage
+        const storageRef = ref(storage, `FotosPerfis/${user.uid}/${photo.name}`)
+        uploadBytes(storageRef, photo)
+            .then(() => {
 
+                // Obter a URL do download da imagem
+                getDownloadURL(storageRef)
+                    .then((downloadURL) => {
+
+                        const informacoesAtualizadas = {
+                            displayName: `${firstName} ${lastName}`,
+                            photoURL: downloadURL
+                        };
+
+                        if (user !== null) {
+                            // Atualizar o perfil do usuário com as informações e URL da imagem
+                            updateProfile(user, informacoesAtualizadas)
+                                .then(() => {
+                                    callback("Conta Atualizada com Sucesso!")
+                                })
+                                .catch((error) => console.log("Não foi possível adicionar as infos no perfil do usuário: ", error))
+                        }
+                    })
+                    .catch((error) => console.log("Não foi possível obter a URL da imagem: ", error))
+            })
+            .catch((error) => console.log("Não foi possível realizar o upload do arquivo: ", error))
     }
+
 
     static deleteAccount = (callback) => {
 
         const auth = getAuth()
         const user = auth.currentUser
 
-        if (user !== null){
+        if (user !== null) {
             deleteUser(user)
                 .then(() => {
                     callback("Conta Deletada com Sucesso!")
