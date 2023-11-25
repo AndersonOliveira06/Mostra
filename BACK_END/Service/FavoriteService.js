c085b51bef93196f0524b52b5ee0ac9b577d2118
Yuri Lima <yuriibrasil@gmail.com>
2023-11-25 09:48:22 -0300

    teste de conexão

---
+++ BACK_END/Service/FavoriteService.js
@@ -0,0 +1,113 @@
+import { getFirestore, collection, addDoc, query, where, getDocs, deleteDoc } from "firebase/firestore";
+import { getAuth } from "firebase/auth";
+
+class FavoriteService {
+
+    static addFavoritePlace = (pontoTuristicoId, callback) => {
+        const db = getFirestore()
+        const lugaresFavoritosCollection = collection(db, "favoritos")
+
+        const auth = getAuth()
+        const user = auth.currentUser
+        const userUID = user.uid.toString()
+
+        if (user !== null){
+            const novoLocalFavorito = {
+                star_id: pontoTuristicoId,
+                user_id: userUID,
+            };
+    
+            addDoc(lugaresFavoritosCollection, novoLocalFavorito)
+                .then(
+                    (document) => {
+                        callback(`Local favorito com id ${document.id} adicionado com sucesso pelo usuário com id ${userUID}`)
+                    }
+                )
+                .catch((error) => console.log("Erro ao adicionar local favorito: ", error))
+        }
+
+        console.log("Não existe usuário logado")
+    }
+
+    static getLugaresFavoritosByUser = (callback) => {
+        const db = getFirestore()
+        const lugaresFavoritosCollection = collection(db, "favoritos")
+
+        const auth = getAuth()
+        const user = auth.currentUser
+        const userUID = user.uid.toString()
+
+        if (user !== null){
+            const q = query(lugaresFavoritosCollection, where("user_id", "==", userUID))
+    
+            getDocs(q)
+                .then(
+                    (snapshot) => {
+                        const lugaresFavoritos = []
+                        const lugaresTuristicosCollection = collection(db, "star")
+    
+                        snapshot.forEach(
+                            (document) => {
+                                //const id = document.id
+                                const { star_id, user_id } = document.data()
+    
+                                const c = query(lugaresTuristicosCollection, where("id", "==", star_id))
+    
+                                getDocs(c)
+                                    .then(
+                                        (querySnapshot) => {
+                                            const doc = querySnapshot.docs[0]
+                                            const { descricao, fotos, localizacao, nome } = doc.data()
+                                            lugaresFavoritos.push({ star_id, user_id, nome, descricao, fotos, localizacao })
+                                        }
+                                    )
+                                    .catch((error) => console.log("Erro ao comparar os IDs: ", error))
+                            }
+                        )
+
+                        callback(lugaresFavoritos)
+                    }
+                )
+                .catch(error => console.log("Erro ao tentar pegar os dados do Firestore: ", error))
+        }
+
+        console.log("Não existe usuário logado")
+    }
+
+    static deleteLugaresFavoritosByUser = (pontoTuristicoId, callback) => {
+        const db = getFirestore()
+        const lugaresFavoritosCollection = collection(db, "favoritos")
+
+        const auth = getAuth()
+        const user = auth.currentUser
+        const userUID = user.uid.toString()
+
+        if (user !== null) {
+            const q = query(lugaresFavoritosCollection, where("user_id", "===", userUID), where("star_id", "==", pontoTuristicoId))
+            
+            getDocs(q)
+                .then(
+                    (snapshot) => {
+    
+                        if (snapshot.size === 0) {
+                            return ("Nenhum documento encontrado para exclusão.")
+                        }
+    
+                        const doc = snapshot.docs[0]
+                        deleteDoc(doc.ref)
+                            .then(() => {
+                                callback(`Local favorito com id ${doc.id} deletado com sucesso pelo usuário com id ${userUID}`)
+                            })
+                            .catch((error) => console.log("Erro ao deletar local favorito: ", error))
+    
+                    }
+                )
+                .catch(error => console.log("Erro ao tentar pegar os dados do Firestore: ", error))
+        }
+
+        console.log("Não existe usuário logado")
+    }
+
+}
+
+export default FavoriteService
---
+++ BACK_END/Service/UsuarioService.js
@@ -0,0 +1,126 @@
+import { createUserWithEmailAndPassword, signInWithEmailAndPassword, getAuth, updateProfile, deleteUser } from "firebase/auth"
+import { storage } from "../firebase/firebase_config"
+import { ref, getDownloadURL, uploadBytes } from "firebase/storage"
+
+class UsuarioService {
+
+    static signUp = (auth, email, password, firstName, lastName, photo, callback) => {
+
+        createUserWithEmailAndPassword(
+            auth,
+            email,
+            password
+        )
+            .then((userCredential) => {
+                const user = userCredential.user
+
+                // Upload da imagem para o Storage
+                const storageRef = ref(storage, `FotosPerfis/${user.uid}/${photo.name}`)
+                uploadBytes(storageRef, photo)
+                    .then(() => {
+
+                        // Obter a URL do download da imagem
+                        getDownloadURL(storageRef)
+                            .then((downloadURL) => {
+
+                                const informacoesAtualizadas = {
+                                    displayName: `${firstName} ${lastName}`,
+                                    photoURL: downloadURL
+                                }
+
+                                // Atualizar o perfil do usuário com as informações e URL da imagem
+                                updateProfile(user, informacoesAtualizadas)
+                                    .then(() => {
+                                        callback(userCredential)
+                                    })
+                                    .catch((error) => console.log("Não foi possível adicionar as infos no perfil do usuário: ", error))
+
+                            })
+                            .catch((error) => console.log("Não foi possível obter a URL da imagem: ", error))
+                    })
+                    .catch((error) => console.log("Não foi possível realizar o upload do arquivo: ", error))
+            })
+            .catch((error) => console.log("Não foi possível cadastrar usuário: ", error))
+    }
+
+
+    static signIn = (auth, email, password, callback) => {
+
+        signInWithEmailAndPassword(
+            auth,
+            email,
+            password
+        )
+            .then((userCredential) => {
+                callback(userCredential)
+            })
+            .catch((error) => console.log(error))
+    }
+
+
+    static updateAccountData = (firstName, lastName, photo, callback) => {
+
+        const auth = getAuth()
+        const user = auth.currentUser
+
+        // Upload da imagem para o Storage
+        const storageRef = ref(storage, `FotosPerfis/${user.uid}/${photo.name}`)
+        uploadBytes(storageRef, photo)
+            .then(() => {
+
+                // Obter a URL do download da imagem
+                getDownloadURL(storageRef)
+                    .then((downloadURL) => {
+
+                        const informacoesAtualizadas = {
+                            displayName: `${firstName} ${lastName}`,
+                            photoURL: downloadURL
+                        };
+
+                        if (user !== null) {
+                            // Atualizar o perfil do usuário com as informações e URL da imagem
+                            updateProfile(user, informacoesAtualizadas)
+                                .then(() => {
+                                    callback("Conta Atualizada com Sucesso!")
+                                })
+                                .catch((error) => console.log("Não foi possível adicionar as infos no perfil do usuário: ", error))
+                        }
+                    })
+                    .catch((error) => console.log("Não foi possível obter a URL da imagem: ", error))
+            })
+            .catch((error) => console.log("Não foi possível realizar o upload do arquivo: ", error))
+    }
+
+
+    static deleteAccount = (callback) => {
+
+        const auth = getAuth()
+        const user = auth.currentUser
+
+        if (user !== null) {
+            deleteUser(user)
+                .then(() => {
+                    callback("Conta Deletada com Sucesso!")
+                })
+                .catch((error) => console.log(error))
+        }
+
+    }
+
+    static getAccountInfo = () => {
+
+        const auth = getAuth()
+        const user = auth.currentUser
+
+        if (user !== null) {
+            return {
+                displayName: user.displayName,
+                email: user.email,
+                photoURL: user.photoURL,
+                uid: user.uid
+            }
+        }
+    }
+}
+
+export default UsuarioService
